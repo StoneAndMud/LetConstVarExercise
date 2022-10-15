2245f60f912b55e383266d71f70b89d1deb4fbe2
Pedro <pedrob@Pedros-MacBook-Pro.local>
2022-10-15 13:33:36 -0500

    first commit

---
+++ LetConstVarExercise.js
@@ -0,0 +1,21 @@
+// ES5 Global Constants
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+// ES2015 Global Constants
+/* Write an ES2015 Version */
+const PI = 3.14;
+PI = 42;
+
+// Quiz
+// What is the difference between var and let?
+// Var is function scoped, that can be reassigned, redeclared and can mutate, while Let is block scoped, can be reassigned, can Not be redeclared and can mutate.
+
+// What is the difference between var and const?
+// Var is function scoped, that can be reassigned, redeclared and can mutate, while Const is block scoped, can Not be reassigned, can Not be redeclared and can mutate.
+
+// What is the difference between let and const?
+// Let is block scoped, can be reassigned, can Not be redeclared and can mutate, while Const is block scoped, can Not be reassigned, can Not be redeclared and can mutate.
+
+// What is hoisting?
+// Variable Hoisting is specific to Var's which when declared they are actually run first, as if it was at the top of the file. So it may seem to not work when looking at the code but actually will return undefined if the var was not defined.
